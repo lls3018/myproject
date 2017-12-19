30a1ff80aa4e41767ca06581fc538c00ed2d9289
lls3018 <lls3018@163.com>
2017-12-19 17:19:51 +0800

    add TensorFlow test

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ __author__ = 'leon'
 
 
 from numpy import *
+import tensorflow as tf
 arr = random.rand(4,4)
 arr1 = mat(arr)
 #print "arr1",arr1
@@ -23,6 +24,6 @@ data = mat([1,2,3,4])
 import numpy as np
 #print np.dot([1,2,3], [4,5,6])
 #print np.ones(5)
-print np.random.random((3,3))
-import pybrain
+#print np.random.random((3,3))
+print('11111111111111')
 
