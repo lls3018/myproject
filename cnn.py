e34063f717b0eb99a214c9f6b571118f6c24c51d
lls3018 <lls3018@163.com>
2018-01-05 17:52:34 +0800

    update directory

--- cnn.py
+++ cnn.py
@@ -1,6 +1,9 @@
 # -*- coding:utf-8 -*-
 __author__ = 'leon'
 
+def func(x):
+    return x*x
+
 
 class ConLayer(object):
     '''
@@ -9,12 +12,13 @@ class ConLayer(object):
     def __init__(self):
         pass
 
-    def conv(self):
-        # 对数据图片进行卷积操作
-        pass
+    def conv(self, data, params):
+        out_list = []
+        for p in params:
+            out_list.append(func(conv(data, p['kernel'], p['bias'])))
+        return out_list
 
     def gradient(self):
-        # 计算梯度
         pass
 
 
@@ -26,6 +30,22 @@ class PoolingLayer(object):
     def __init__(self):
         pass
 
+    def mean(self, input):
+        out = func( mean(input()) * w + b )
+        return out
+
 
 if __name__=='__main__':
-    #
+    p_data = []
+    # 参数需要初始化
+    params = [{'kernel': [[1,1],[1,1]],'bias': []}]
+    con = ConLayer()
+    pool = PoolingLayer()
+    out = con.conv(p_data, params)
+    out = pool.mean(out)
+
+
+
+
+
+
